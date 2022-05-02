67ce586c707093223bc6ea322aa45b4fbbae23f8
Krishna Panjiyar <krishna.panjiyar789@gmail.com>
2022-05-01 21:16:56 -0700

    Update Insertions.sql

--- Insertions.sql
+++ Insertions.sql
@@ -144,3 +144,97 @@ VALUES (4, 4, 200, "Aisle 3, Cabinet 2");
 INSERT INTO pharmacy.Inventory (DRUG_ID, MANUFACTURER_ID, QUANTITY, LOCATION) 
 VALUES (5, 2, 200, "Aisle 2, Cabinet 1");
 
+
+--
+-- Dumping data for table `Doctor`
+--
+
+INSERT INTO `Doctor` (`ID`, `FIRST_NAME`, `LAST_NAME`, `PHONE_NUM`, `HOSPITAL_ID`) VALUES
+(1, 'Wade', 'Avery', '1-116-565-1167', 1),
+(2, 'Payton', 'Lynn', '2-286-745-2270', 2),
+(3, 'Rocco', 'Allington', '0-225-702-3610', 3),
+(4, 'Stephanie', 'Redwood', '3-578-584-7264', 4),
+(5, 'Enoch', 'Jarrett', '7-260-740-8421', 5),
+(6, 'Charlotte', 'Lambert', '5-755-638-5008', 6),
+(7, 'Rosie', 'Ainsworth', '0-400-234-3403', 7),
+(8, 'Margot', 'Hobbs', '1-575-751-3080', 8),
+(9, 'Moira', 'Ianson', '2-508-877-8106', 9),
+(10, 'Janice', 'Shaw', '3-513-244-4220', 10);
+
+--
+-- Dumping data for table `Drug`
+--
+
+INSERT INTO `Drug` (`ID`, `NAME`, `DATE_OF_MANUFACTURE`, `EXPIRATION_DATE`, `DRUGTYPE`, `MANUFACTURER_ID`, `MANUFACTURING_COST`, `DESCRIPTION`) VALUES
+(13, 'Neproxane', '2021-10-04', '2022-11-25', 'OxyContin', 51795, 15, 'Best Relief for pain, fever, anxiety, cold'),
+(15, 'Ibuprofen', '2020-10-04', '2023-10-04', 'Zoloft', 59354, 12, 'Best Relief for pain, fever, anxiety, cold'),
+(16, 'Mexanone', '2021-10-04', '2022-11-25', 'Cosopt', 51795, 9, 'Best for burn, cold, flu, and allergies'),
+(19, 'Micoglcium', '2020-10-04', '2024-10-04', 'Ibalgin', 59355, 8, 'Best for burn, cold, flu, and allergies'),
+(21, 'Methoxyl500', '2021-10-04', '2024-10-04', 'Claritin', 64683, 16, 'Best for burn, cold, flu, and allergies');
+
+--
+-- Dumping data for table `Hospital`
+--
+
+INSERT INTO `Hospital` (`ID`, `NAME`, `ADDRESS`, `CITY`, `STATE`, `ZIPCODE`, `PHONE_NUM`, `EMAIL`) VALUES
+(13, 'Carbon health', '28 N Almaden Ave #10', 'San Jose', 'CA', 95110, 1085161949, 'ucare@health.com'),
+(15, 'SCV Medical Center', '761 E Santa Clara St', 'San Jose', 'CA', 95112, 1084161949, 'svc@health.com'),
+(21, 'Regional Hospital', '225 N Jackson Ave', 'San Jose', 'CA', 95116, 156541554, 'RMC@med.com'),
+(55, 'O\'Connor Hospital', '105 Forest Ave', 'San Jose', 'CA', 95128, 408256716, 'OC@hospital.com'),
+(64, 'McKee Hospital', '2360 McKee Rd', 'San Jose', 'CA', 95116, 1664846515, 'MMC@health.com');
+
+--
+-- Dumping data for table `Presecription`
+--
+
+INSERT INTO `Presecription` (`RX_NUMBER`, `CONSUMER_ID`, `DRUG_ID`, `DATE_CREATED`, `EXPIRATION_DATE`, `QUANTITY`, `NUMBER_OF_REFILLS`, `ADDITIONAL_INFORMATION`, `DOCTOR_ID`) VALUES
+(1520, 16, 19, '2021-07-07', '2023-07-07', 10, 4, '', 3),
+(1521, 17, 16, '2021-07-07', '2025-07-07', 12, 5, '', 4),
+(1522, 19, 15, '2020-07-07', '2025-05-07', 15, 4, '', 1),
+(1523, 18, 16, '2021-07-07', '2024-07-07', 16, 8, '', 4),
+(1525, 15, 15, '2021-07-07', '2022-07-07', 18, 5, '', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Doctor`
+--
+ALTER TABLE `Doctor`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `PHONE_NUM` (`PHONE_NUM`);
+
+--
+-- Indexes for table `Drug`
+--
+ALTER TABLE `Drug`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `Hospital`
+--
+ALTER TABLE `Hospital`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `PHONE_NUMBER` (`PHONE_NUM`),
+  ADD UNIQUE KEY `EMAIL` (`EMAIL`);
+
+--
+-- Indexes for table `Presecription`
+--
+ALTER TABLE `Presecription`
+  ADD PRIMARY KEY (`RX_NUMBER`),
+  ADD KEY `DRUG_ID` (`DRUG_ID`),
+  ADD KEY `DOCTOR_ID` (`DOCTOR_ID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Presecription`
+--
+ALTER TABLE `Presecription`
+  ADD CONSTRAINT `DOCTOR_ID` FOREIGN KEY (`DOCTOR_ID`) REFERENCES `Doctor` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `DRUG_ID` FOREIGN KEY (`DRUG_ID`) REFERENCES `Drug` (`ID`) ON UPDATE CASCADE;
+COMMIT;
